9a5ef90948d0945d5f2e8e7250f29a26dad51a19
Davor <davor.kos@gmail.com>
2024-06-23 19:17:55 +0200

    meter.py created

---
+++ meter.py
@@ -0,0 +1,24 @@
+from tkinter import *
+import ttkbootstrap as tb
+from PIL import Image
+
+Image.CUBIC = Image.BICUBIC
+
+root = tb.Window(themename="superhero")
+
+# root = Tk()
+root.title("TTK Boostrap! Meter")
+root.iconbitmap("images/codemy.ico")
+root.geometry("500x350")
+
+my_meter = tb.Meter(
+    root,
+    bootstyle="danger",
+    subtext="Tkinter Learned",
+    subtextfont=("Helvetica", 14),
+    interactive=True,
+    textright="$"
+)
+my_meter.pack(pady=50)
+
+root.mainloop()
