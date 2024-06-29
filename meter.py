687584cd0969efd3a9920b21cf26a5d78ab5ac3d
Davor <davor.kos@gmail.com>
2024-06-29 19:39:54 +0200

    sync

--- meter.py
+++ meter.py
@@ -11,15 +11,45 @@ root.title("TTK Boostrap! Meter")
 root.iconbitmap("images/codemy.ico")
 root.geometry("500x350")
 
+
+def up():
+    my_meter.step(10)
+
+
+def down():
+    my_meter.step(-10)
+
+
+global counter
+counter = 5
+
+
+def clicker():
+    global counter
+    my_meter.configure(amountused=counter)
+    counter += 5
+    my_button.configure(text=f'Click Me {my_meter.amountusedvar.get()}')
+
+
 my_meter = tb.Meter(
     root,
     bootstyle="danger",
     subtext="Tkinter Learned",
     subtextfont=("Helvetica", 14),
     interactive=True,
-    textright="$",
+    textright="%",
     stripethickness=10,
+    subtextstyle="danger"
 )
-my_meter.pack(pady=50)
+my_meter.pack(pady=10)
+
+my_button = tb.Button(root, text="My Button", command=clicker)
+my_button.pack(pady=5)
+
+my_buttonUp = tb.Button(root, text="Step Up", command=up)
+my_buttonUp.pack(pady=5)
+
+my_buttonDown = tb.Button(root, text="Step Down", command=down)
+my_buttonDown.pack(pady=5)
 
 root.mainloop()
---
+++ notebook.py
@@ -0,0 +1,14 @@
+from tkinter import *
+from ttkbootstrap.constants import *
+import ttkbootstrap as tb
+
+root=tb.Window(themename="superhero")
+#root = Tk()
+root.title("TTK Boostrap!")
+root.iconbitmap('images/codemy.ico')
+root.geometry('500x350')
+
+
+
+
+root.mainloop()
